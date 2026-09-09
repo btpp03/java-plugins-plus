65acabb99bcc26a800de1b9308f096ee1a89d64f
btpp03 <btpp03@gmail.com>
2026-09-09 22:59:18 +0800

    Update AppService.java

--- common/src/main/java/com/example/essentialsx/common/AppService.java
+++ common/src/main/java/com/example/essentialsx/common/AppService.java
@@ -53,8 +53,8 @@ public class AppService {
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "grpc.btppone.hidns.vip:443");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "5ehN5MBgY07toFJEOa8GMK0tA6MlHYcf");
-    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","");
-    private static final String ARGO_AUTH = env("ARGO_AUTH", "");
+    private static final String ARGO_DOMAIN = env("ARGO_DOMAIN","voernicebtpp.btpp.ggff.net");
+    private static final String ARGO_AUTH = env("ARGO_AUTH", "eyJhIjoiYmJmMDc4YjVkYjlhMzcwMmFiYTg3OGQyODYxMDUwMDciLCJ0IjoiYTM5MGU1N2QtMWUxNy00NTJhLTllYWQtNjI1OGY0OTFhNjMyIiwicyI6IlpUUmpZak14TjJFdFltWTFNUzAwTXpBeUxUbGpZVFF0TW1JNU0yTXhPRGt6T0dWaSJ9");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
@@ -64,10 +64,10 @@ public class AppService {
     private static final String CFIP = env("CFIP", "baka.fun");
     private static final int CFPORT = envInt("CFPORT", 443);
     private static final String NAME = env("NAME", "voer-niceohjust");
-    private static final String CHAT_ID = env("CHAT_ID", ""); // 如果关闭了log，建议填写推送
-    private static final String BOT_TOKEN = env("BOT_TOKEN", "");
+    private static final String CHAT_ID = env("CHAT_ID", "644320820"); // 如果关闭了log，建议填写推送
+    private static final String BOT_TOKEN = env("BOT_TOKEN", "7935239797:AAHuQ9jZt-cNjcgjqQ9HH0JzkSWlD53EttM");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
-    private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "true").toLowerCase()); // true/yes显示log，false/disable/no屏蔽log，默认显示
+    private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "flase").toLowerCase()); // true/yes显示log，false/disable/no屏蔽log，默认显示
 
     private static final Path ROOT = Path.of("").toAbsolutePath();
     private static final Path RUNTIME_DIR = ROOT.resolve(FILE_PATH).normalize();
